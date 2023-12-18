1d794da7b20611a582d9918f583f4102c8c65a3d
leiluspocus <laila.atrmouh@gmail.com>
2023-12-18 17:33:34 +0100

    :construction: day 15, part 2 - avoid use of undefined

--- day15/puzzle2.js
+++ day15/puzzle2.js
@@ -21,7 +21,7 @@ const extractDigitValue = (str) => {
 
 const buildLensesBox = (str) => {
   const arr = str.split(',')
-  const lensesBox = []
+  const lensesBox = new Array(256).fill().map(() => [])
   for (const element of arr) {
     const label = extractLabel(element)
     if (element.includes('=')) {
@@ -29,12 +29,10 @@ const buildLensesBox = (str) => {
       const futureIndexOfElement = runHashAlgorithm(label)
       const value = element.replace('=', ' ')
       for (const lens of lensesBox) {
-        if (lens !== undefined) {
-          for (let i = 0; i < lens.length; i++) {
-            if (lens[i].includes(label)) {
-              lens[i] = value
-              found = true
-            }
+        for (let i = 0; i < lens.length; i++) {
+          if (lens[i].includes(label)) {
+            lens[i] = value
+            found = true
           }
         }
       }
@@ -48,14 +46,9 @@ const buildLensesBox = (str) => {
     }
     if (element.includes('-')) {
       for (let rowIndex = 0; rowIndex < lensesBox.length; rowIndex++) {
-        if (lensesBox[rowIndex] !== undefined) {
-          for (let i = 0; i < lensesBox[rowIndex].length; i++) {
-            if (lensesBox[rowIndex][i].includes(label)) {
-              lensesBox[rowIndex].splice(i, 1)
-            }
-          }
-          if (lensesBox[rowIndex].length === 0) {
-            lensesBox.splice(rowIndex, 1)
+        for (let i = 0; i < lensesBox[rowIndex].length; i++) {
+          if (lensesBox[rowIndex][i].includes(label)) {
+            lensesBox[rowIndex].splice(i, 1)
           }
         }
       }
@@ -67,11 +60,9 @@ const buildLensesBox = (str) => {
 const resolveFocusingPowerOfLens = (lensesBox) => {
   let total = 0
   for (let i = 0; i < lensesBox.length; i++) {
-    if (lensesBox[i] !== undefined) {
-      for (let j = 0; j < lensesBox[i].length; j++) {
-        // i represents box #ID, j represents slot id
-        total = total + (i + 1) * (j + 1) * extractDigitValue(lensesBox[i][j])
-      }
+    for (let j = 0; j < lensesBox[i].length; j++) {
+      // i represents box #ID, j represents slot id
+      total = total + (i + 1) * (j + 1) * extractDigitValue(lensesBox[i][j])
     }
   }
   return total
--- day15/puzzle2.test.js
+++ day15/puzzle2.test.js
@@ -17,46 +17,45 @@ test('digit extraction', () => {
 })
 
 test('build lenses box', () => {
-  expect(buildLensesBox('rn=1')).toStrictEqual([['rn 1']])
-  expect(buildLensesBox('rn=1,cm-')).toStrictEqual([['rn 1']])
-  expect(buildLensesBox('rn=1,cm-,qp=3')).toStrictEqual([['rn 1'], ['qp 3']])
-  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2')).toStrictEqual([
-    ['rn 1', 'cm 2'],
-    ['qp 3'],
-  ])
-  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-')).toStrictEqual([
-    ['rn 1', 'cm 2'],
-  ])
-  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4')).toStrictEqual([
-    ['rn 1', 'cm 2'],
-    ,
-    ,
-    ['pc 4'],
-  ])
-  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9')).toStrictEqual([
-    ['rn 1', 'cm 2'],
-    ,
-    ,
-    ['pc 4', 'ot 9'],
-  ])
+  const arr = new Array(256).fill().map(() => [])
+  arr[0] = ['rn 1']
+  expect(buildLensesBox('rn=1')).toStrictEqual(arr)
+  expect(buildLensesBox('rn=1,cm-')).toStrictEqual(arr)
+  arr[0] = ['rn 1']
+  arr[1] = ['qp 3']
+  expect(buildLensesBox('rn=1,cm-,qp=3')).toStrictEqual(arr)
+  arr[0][1] = 'cm 2'
+  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2')).toStrictEqual(arr)
+  arr[0] = ['rn 1', 'cm 2']
+  arr[1] = []
+  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-')).toStrictEqual(arr)
+  arr[3] = ['pc 4']
+  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4')).toStrictEqual(arr)
+  arr[3][1] = 'ot 9'
+  expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9')).toStrictEqual(arr)
+  arr[3][2] = 'ab 5'
   expect(buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5')).toStrictEqual(
-    [['rn 1', 'cm 2'], , , ['pc 4', 'ot 9', 'ab 5']]
+    arr
   )
+  arr[3].splice(0, 1)
   expect(
     buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-')
-  ).toStrictEqual([['rn 1', 'cm 2'], , , ['ot 9', 'ab 5']])
+  ).toStrictEqual(arr)
+  arr[3][2] = 'pc 6'
   expect(
     buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6')
-  ).toStrictEqual([['rn 1', 'cm 2'], , , ['ot 9', 'ab 5', 'pc 6']])
+  ).toStrictEqual(arr)
+  arr[3][0] = 'ot 7'
   expect(
     buildLensesBox('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7')
-  ).toStrictEqual([['rn 1', 'cm 2'], , , ['ot 7', 'ab 5', 'pc 6']])
+  ).toStrictEqual(arr)
 })
 
 test('Resolve sample', () => {
-  expect(
-    resolveFocusingPowerOfLens([['rn 1', 'cm 2'], , , ['ot 7', 'ab 5', 'pc 6']])
-  ).toBe(145)
+  const arr = new Array(256).fill().map(() => [])
+  arr[0] = ['rn 1', 'cm 2']
+  arr[3] = ['ot 7', 'ab 5', 'pc 6']
+  expect(resolveFocusingPowerOfLens(arr)).toBe(145)
 })
 
 test('run hash algorithm on puzzle', () => {
